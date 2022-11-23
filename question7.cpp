b2e0fe4c2f1be632bb8d661964ec7b17c2b63283
DongHwi999 <musicaro10201@gmail.com>
2022-11-23 21:11:17 +0900

    큐

---
+++ question7.cpp
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE    100
+
+typedef int Element;
+Element data[MAX_QUEUE_SIZE];   
+int    front;        
+int    rear;           
+
+void error(char* str)
+{
+    fprintf(stderr, "%s\n", str);
+    exit(1);
+};
+void init_queue() { front = rear = 0; ; }
+int is_empty() { return front == rear; }
+int is_full() { return front == (rear + 1) % MAX_QUEUE_SIZE; }
+int size() { return(rear - front + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE; }
+
+void enqueue(Element val)
+{
+    if (is_full())
+        error("  Å¥ Æ÷È­ ¿¡·¯");
+    rear = (rear + 1) % MAX_QUEUE_SIZE;
+    data[rear] = val;
+}
+Element dequeue()
+{
+    if (is_empty())
+        error("  Å¥ °ø¹é ¿¡·¯");
+    front = (front + 1) % MAX_QUEUE_SIZE;
+    return data[front];
+}
+Element peek()
+{
+    if (is_empty())
+        error("  Å¥ °ø¹é ¿¡·¯");
+    return data[(front + 1) % MAX_QUEUE_SIZE];
+}
+
+
+void init_deque() { init_queue(); }
+void add_rear(Element val) { enqueue(val); }
+Element delete_front() { return dequeue(); }
+Element get_front() { return peek(); }
+
+void add_front(Element val)
+{
+    if (is_full())
+        error("  µ¦ Æ÷È­ ¿¡·¯");
+    data[front] = val;
+    front = (front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+}
+
+Element delete_rear()
+{
+    int prev = rear;
+    if (is_empty())
+        error("  µ¦ °ø¹é ¿¡·¯");
+    rear = (rear - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+    return data[prev];
+}
+
+Element get_rear()
+{
+    if (is_empty())
+        error("  µ¦ °ø¹é ¿¡·¯");
+    return data[rear];
+}
+
+void print_queue(char msg[]) {
+    int i, maxi = rear;
+    if (front >= rear) maxi += MAX_QUEUE_SIZE;
+    printf("%s[%2d]= ", msg, size());
+    for (i = front + 1; i <= maxi; i++)
+        printf("%2d ", data[i%MAX_QUEUE_SIZE]);
+    printf("\n");
+}
+
+void print_deque(char msg[]) { print_queue(msg); }
+
+int main()
+{
+    int i;
+    
+    init_deque();
+    for (i = 1; i<10; i++) {
+        if (i % 2) add_front(i);
+        else add_rear(i);
+    }
+    print_deque("¿øÇü µ¦ È¦¼ö-Â¦¼ö  ");
+    printf("\tdelete_front() --> %d\n", delete_front());
+    printf("\tdelete_rear () --> %d\n", delete_rear());
+    printf("\tdelete_front() --> %d\n", delete_front());
+    print_deque("¿øÇü µ¦ »èÁ¦-È¦Â¦È¦");
+    
+    return 0;
+}
