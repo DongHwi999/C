5be39341bef102190d0bbaae16f22a2715b478a6
DongHwi999 <117573896+DongHwi999@users.noreply.github.com>
2023-02-08 23:54:42 +0900

    알파벳

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int a;
+	int t; 
+   for (int a='a'; i<='z'; i++) {
+	   if (!(a=='a' || a=='e' || a=='i' || a=='o' || a=='u')) {
+		   printf("%c", a);
+	   }   
+   }
+   return 0;
+}
